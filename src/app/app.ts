364023528b3487be8787617a369ae019cbcd839a
Konstantin Vengerov <kvengerov@gmail.com>
2025-10-10 13:28:01 +0300

    added header components, basic routing

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,13 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Header } from './layout/header/header';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Header],
   template: `
-    <h1 class="bg-red-500">Welcome to {{ title() }}!</h1>
-
-    <router-outlet />
+    <app-header/>
+    <router-outlet/>
   `,
   styles: [],
 })
